e3f969740c73da511941fc472dbc7c2ff78c1b8d
NavhiAngeles <ivhanangeles.04@gmail.com>
2025-10-20 19:21:54 +0800

    still missing  understanding for what going to do for the button FIND

--- ReadingAFile/FrmStudentRecord.Designer.cs
+++ ReadingAFile/FrmStudentRecord.Designer.cs
@@ -31,9 +31,9 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmStudentRecord));
             this.listView1 = new System.Windows.Forms.ListView();
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.btnUpload = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listView1
@@ -57,38 +57,41 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "View Record";
             // 
-            // button1
+            // btnRegister
             // 
-            this.button1.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(510, 80);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(70, 34);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Register";
-            this.button1.UseVisualStyleBackColor = false;
+            this.btnRegister.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btnRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegister.Location = new System.Drawing.Point(510, 80);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(70, 34);
+            this.btnRegister.TabIndex = 2;
+            this.btnRegister.Text = "Register";
+            this.btnRegister.UseVisualStyleBackColor = false;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
             // 
-            // button2
+            // btnFind
             // 
-            this.button2.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(510, 135);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(70, 34);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Find";
-            this.button2.UseVisualStyleBackColor = false;
+            this.btnFind.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btnFind.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFind.Location = new System.Drawing.Point(510, 135);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(70, 34);
+            this.btnFind.TabIndex = 3;
+            this.btnFind.Text = "Find";
+            this.btnFind.UseVisualStyleBackColor = false;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
             // 
-            // button3
+            // btnUpload
             // 
-            this.button3.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(510, 188);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(70, 34);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "Upload";
-            this.button3.UseVisualStyleBackColor = false;
+            this.btnUpload.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btnUpload.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpload.Location = new System.Drawing.Point(510, 188);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(70, 34);
+            this.btnUpload.TabIndex = 4;
+            this.btnUpload.Text = "Upload";
+            this.btnUpload.UseVisualStyleBackColor = false;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
             // 
             // FrmStudentRecord
             // 
@@ -97,9 +100,9 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(596, 369);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnUpload);
+            this.Controls.Add(this.btnFind);
+            this.Controls.Add(this.btnRegister);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.listView1);
             this.Name = "FrmStudentRecord";
@@ -113,8 +116,8 @@
 
         private System.Windows.Forms.ListView listView1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.Button btnUpload;
     }
 }
--- ReadingAFile/FrmStudentRecord.cs
+++ ReadingAFile/FrmStudentRecord.cs
@@ -12,9 +12,28 @@ namespace ReadingAFile
 {
     public partial class FrmStudentRecord : Form
     {
+        FrmRegistration frmReg = new FrmRegistration();
+
         public FrmStudentRecord()
         {
             InitializeComponent();
+        }
+
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Succesfully Uploaded!");
+            
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            frmReg.Show();
+            this.Hide();
+        }
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
